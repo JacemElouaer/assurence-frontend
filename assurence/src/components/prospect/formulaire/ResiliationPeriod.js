b25cff22daeba022ae7df21c359574624b9a6fba
jasem-elwaar <jassem.elwaar@gmail.com>
2022-02-27 14:21:06 +0100

    finish resiliation date

--- assurence/src/components/prospect/formulaire/ResiliationPeriod.js
+++ assurence/src/components/prospect/formulaire/ResiliationPeriod.js
@@ -5,9 +5,7 @@ import SelectLabels from '../../utils/SelectLabels'
 import  {saveProgress} from '../../../redux/actions/formsData'
 import {saveResiliationPeriode} from '../../../redux/actions/formsData' ; 
 import {useDispatch ,  useSelector} from  'react-redux'
-import RadioGroup from '@mui/material/RadioGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import StyledRadio from "../../utils/StyledRadio";
+
 
 let option   =  ["Janvier" , "Fevrier" ,  "Mars" ,  "Avril" ,  "Mai" , "Join" , "Juillet" ,  "Aout" , "Septembre" , "Octobre", "Novembre ", "Decembre"]
  
@@ -28,44 +26,24 @@ let bdate =  data.ResiliationP ?   data.ResiliationP.dateres :  ""
 
   let [periode ,  setChoix] =  useState(bchoix) 
   let [date_res ,  setDateRes] = useState(bdate)
-  let  [choisen_year ,  setChoisenYear] = useState("")
-
-  let [month ,  setMonth] =  useState("")
-  let [year ,  setYear] =  useState("") 
- 
-  let current_day  =d.getDate()
-
-  const changeResdate=(m , y)=>{
-    setMonth(m)
-    setYear(y) 
-    setDateRes(`01/${m}/${y}`) 
+ const changeResdate=(d)=>{
+   setDateRes(d)
+   console.log(date_res)
   }
   
 
  useEffect(() => {
-   
   if(periode==="Plus de 12 mois"){
-    setDateRes(`01/${option[mois_courant]}/${year_courant}`)
-  } 
-  
-})
-const  handleYear = (e)=>{
-  setChoisenYear(e.target.value)
-  if(e.target.value == year_courant){
-    setDateRes(`01/${month}/${year_courant+1}`)
-  }
-  if(e.target.value == year_courant-1){ 
     let date = new Date();
-    date.setDate(date.getDate() + 32);  
-    setDateRes( `${date.getDate()}/${option[date.getMonth()]}/${year_courant}`) 
-    console.log(date.getDay() , date)   
-  }
-  console.log(date_res)
+      date.setDate(date.getDate() + 32); 
+      let day  = date.getDate()
+    setDateRes(`${day}/${option[mois_courant]}/${year_courant}`)
+  } 
+})
 
-
-}
 const change= (e)=> {
     if (e.target.id === "Moin de 12 mois"){
+      setDateRes(null)
       setChoix(periode="Moin de 12 mois")
     } 
     if(e.target.id ===  "Plus de 12 mois"){
@@ -79,17 +57,16 @@ const change= (e)=> {
 }
 
 
-
-  
   return (
     <div className="mt-1">
       <div className="">
-      <div className="mx-auto w-full md:w-800  text-center pb-2">
+      <div className="mx-auto w-full md:w-800  text-center">
           <p className="text-xl xs:text-3xl md:text-5xl text-[#444]  text-center  font-sans lg:w-full ">
-          </p>
+          Nous avons besoin de savoir depuis quand vous étiez assuré{ /*pour ce bien afin de définir une date de début à votre contrat*/}
+         </p>
         </div>
       </div>
-      <main className="flex justify-center mt-16">
+      <main className="flex justify-center mt-8">
         <div className="grid grid-cols-1  2xs:grid-cols-2   s:grid-cols-2 gap-5 "  >
         <div onClick={change} id="Moin de 12 mois"> 
         <BinaryOption id="Moin de 12 mois" type="oui" text="type_1" choix={periode}  option_text="Moin de 12 mois"/>
@@ -101,44 +78,35 @@ const change= (e)=> {
       </main>
       <div className="flex  justify-center mx-auto  mt-2">   
     {periode==="Moin de 12 mois"?
-      <div>
       <SelectLabels  setResDate = {changeResdate} mois={bdate.split('/')[1]}  />
-      {date_res ?
-        mois_courant == month  ?  
-        <div className='mt-4'>
-            <RadioGroup
-        onChange={handleYear}
-          row
-          value={choisen_year}
-          aria-labelledby="demo-form-control-label-placement"
-          name="position"
-          defaultValue="top">
-          <FormControlLabel
+   :
+       periode ==="Plus de 12 mois"? <div>
 
-            value={year_courant-1}
-            control={<StyledRadio/>}
-            label={year_courant-1}
-            labelPlacement="top"
-          />
-          <FormControlLabel
-            value={year_courant}
-            control={<StyledRadio/>}
-            label={year_courant}
-            labelPlacement="top"
-          />
-          
-          </RadioGroup>  
-        </div>
-        
-        
-        :  undefined : 
-        undefined}
-      </div>
-       :
-       periode ==="Plus de 12 mois"? <div></div>:undefined 
+        <div class="shadow_box mt-4 text-16 text-default-grey-400 font-sans w-600" >
+      <p>Parfait! La date d’effet de votre contrat sera fixée au {date_res.split('/')[0]}  {date_res.split('/')[1]} {date_res.split('/')[2]} pour prendre en compte la résiliation de votre ancien contrat.
+        <br/>Si nécessaire, vous pourrez modifier cette date ultérieurement.</p>
+           </div>
+
+       </div>:undefined 
 
        }    
-     {/*  <div class="shadow_box mt-4 text-16 text-default-grey-400 font-sans w-600" >
+    
+     </div>
+     {date_res ?<div onClick={senddata}>
+      <Button Suivant="EmailFormulaire" />
+      </div> : undefined }
+      
+   
+    </div>
+  );
+}
+
+export default ResiliationPeriod;
+
+
+
+
+ /*  <div class="shadow_box mt-4 text-16 text-default-grey-400 font-sans w-600" >
  <p>Parfait! La date d’effet de votre contrat sera fixée au 01 {option[month]} {year} pour prendre en compte la résiliation de votre ancien contrat.
            <br/>Si nécessaire, vous pourrez modifier cette date ultérieurement.</p>
            </div> :  undefined}
@@ -147,14 +115,4 @@ const change= (e)=> {
        :  <div class="shadow_box mt-8 text-16 text-default-grey-400 font-sans w-3/4 mx-center" >
        <p>Parfait! La date d’effet de votre contrat sera fixée au 01  {option[mois_courant]} {year_courant} pour prendre en compte la résiliation de votre ancien contrat.
            <br/>Si nécessaire, vous pourrez modifier cette date ultérieurement.</p>
-      </div> */}
-     </div>
-      <div onClick={senddata}>
-      <Button Suivant="EmailFormulaire" />
-      </div>
-   
-    </div>
-  );
-}
-
-export default ResiliationPeriod;
+      </div> */
--- assurence/src/components/utils/SelectLabels.js
+++ assurence/src/components/utils/SelectLabels.js
@@ -1,5 +1,8 @@
 import React ,  { useState} from 'react'; 
 import { useSelector } from 'react-redux';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import StyledRadio from "./StyledRadio";
 export default function SelectLabels(props) {
   let option   =  ["","Janvier" , "Fevrier" ,  "Mars" ,  "Avril" ,  "Mai" , "Join" , "Juillet" ,  "Aout" ,  "Septembre", "Octobre", "Novembre ", "Decembre"]
  
@@ -9,34 +12,110 @@ export default function SelectLabels(props) {
   
     let  [mois ,  setMois] =  useState(0) */
   const d = new Date();
-  let month = d.getMonth()+1; 
-  let year  = d.getFullYear()
+  let current_month = d.getMonth()+1; 
+  let current_year  = d.getFullYear()
+  
+  let [date_res ,  setDateRes]  =  useState(null)
+  let  [choisen_month ,  setMonth] = useState()
+  let  [choisen_year ,  setYear] = useState()
+
+
+
 
   function  onchange(e){
-   
-   e.preventDefault() ;
-   
-    if(e.target.value>month){
-      props.setResDate(e.target.value ,  year)
-    }
-    if(e.target.value<=month){
-      props.setResDate(e.target.value ,  year+1)
+  let date = new Date();
+  let month  = new Date()
+  month.setDate(month.getDate() + 60); 
+  e.preventDefault() ;
+  setMonth(e.target.value)
+    if(e.target.value>d.getMonth()+2){
+    props.setResDate( `1/${e.target.value}/${current_year}`)
+    setDateRes( `1/${e.target.value}/${current_year}`)
+  }
+    if(e.target.value<current_month && e.target.value != 0){
+      props.setResDate( `1/${e.target.value}/${current_year+1}`)
+      setDateRes( `1/${e.target.value}/${current_year+1}`)
     } 
-    if(e.target.value == 0){
-     props.setResDate(null ,null)
+    if (e.target.value ==  month.getMonth() ){
+      let date = new Date();
+    date.setDate(date.getDate() + 32); 
+    let day  = date.getDate()
+    
+    props.setResDate( `${day}/${day.getMonth()+1}/${current_year}`) 
+    setDateRes( `${day}/${day.getMonth()+1}/${current_year}`) 
+   } 
+   if(e.target.value == 0){
+     props.setResDate(null)
+     setDateRes(null)
    }
+  } 
+  const handleYear=(e)=>{
+    if(e.target.value == current_year){
+      props.setResDate(`1/${current_month}/${current_year+1}`)
+      setDateRes(`1/${current_month}/${current_year+1}`)
+    }
+    if(e.target.value == current_year-1){ 
+      let date = new Date();
+      date.setDate(date.getDate() + 32); 
+       
+      props.setResDate( `${date.getDate()}/${date.getMonth()+1}/${current_year}`) 
+      setDateRes( `${date.getDate()}/${date.getMonth()+1}/${current_year}`) 
+
+    }
   }
  
   
   return (
-    <div class="relative">
-        <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+    <div>
+    <div class="flex mx-auto justify-center w-full">
+      <div class="relative">
+        <select class="block appearance-none w-40 bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
          id="grid-state" onChange={onchange}>
           {option.map((m)=><option value= {option.indexOf(m)} key={m}>{m}</option>)}
         </select>
         <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
           <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
         </div>
+        </div>
+    </div>
+        
+        {choisen_month  == current_month ?  
+        <div class="flex  justify-center mt-4">
+              <RadioGroup
+        onChange={handleYear}
+          row
+          value={choisen_year}
+          aria-labelledby="demo-form-control-label-placement"
+          name="position"
+          defaultValue="top">
+          <FormControlLabel
+
+            value={current_year-1}
+            control={<StyledRadio/>}
+            label={current_year-1}
+            labelPlacement="top"
+          />
+          <FormControlLabel
+            value={current_year}
+            control={<StyledRadio/>}
+            label={current_year}
+            labelPlacement="top"
+          />
+          
+          </RadioGroup>  
+
+
+        </div> :  undefined }
+      
+        {date_res ?  
+        <div class="shadow_box mt-4 text-16 text-default-grey-400 font-sans w-600" >
+ <p>Parfait! La date d’effet de votre contrat sera fixée au {date_res.split('/')[0]}  {option[date_res.split('/')[1]]} {date_res.split('/')[2]} pour prendre en compte la résiliation de votre ancien contrat.
+           <br/>Si nécessaire, vous pourrez modifier cette date ultérieurement.</p>
+           </div> :  undefined
+      
+      
+      }
       </div>
+      
   );
 }
